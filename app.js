e35f676c694827501f6cbcb284b75fe98d186798
Thomas Ngo <tngo0508@gmai.com>
2020-06-18 13:13:30 -0700

    using the console

---
+++ app.js
@@ -0,0 +1,24 @@
+// Log to console
+console.log("hello world");
+console.log(123);
+console.log(true);
+var greeting = "hello";
+console.log(greeting);
+console.log([1, 2, 3, 4]);
+console.log({ a: 1, b: 2 });
+console.table({ a: 1, b: 2 });
+
+console.error("this is some error");
+// console.clear();
+
+console.warn("this is a warning");
+console.time("hello");
+console.log("hello world");
+console.log("hello world");
+console.log("hello world");
+console.log("hello world");
+console.log("hello world");
+console.timeEnd("hello");
+/*
+multi line comments
+*/
