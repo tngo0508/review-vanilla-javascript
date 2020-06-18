a226623ab71460985c5f81461e0f710e72ea5e76
Thomas Ngo <tngo0508@gmai.com>
2020-06-18 13:29:43 -0700

    data types in javascript

--- app.js
+++ app.js
@@ -1,51 +1,27 @@
-// var, let, const
+// PRIMITIVE
 
-// var name = "John Doe";
-// console.log(name);
-// name = "Steve Smith";
-// console.log(name);
+// string
+const name = "john doe";
+// number
+const age = 45;
+// boolean
+const hasKids = true;
+// Null
+const car = null;
+// Undefined
+let test;
+// Symbol
+const sym = Symbol();
 
-// // Init var
-// var greeting;
-// console.log(greeting);
-// greeting = "Hello";
-// console.log(greeting);
-
-// // letters, numbers, _, $
-// // Cannot start with number
-// var name = "John";
-
-// Multi word vars
-// var firstName = "John"; // Camel case
-// var firstName = "Sara"; // Underscore
-// var FirstName = "Tom"; // Pascal case
-// var firstname;
-
-// LET
-// let name = "John Doe";
-// console.log(name);
-// name = "Steve Smith";
-// console.log(name);
-
-// // CONST
-// const name = "John";
-// console.log(name);
-// // Cannot reassign
-// // name = 'Sara';
-// // Have to assign a value
-// // const greeting;
-
-const person = {
-  name: "John",
-  age: 30,
+// reference types - objects
+// array
+const hobbies = ["movies", "music"];
+// object literal
+const address = {
+  city: "boston",
+  state: "ma",
 };
 
-person.name = "Sara";
-person.age = 32;
-
-// console.log(person);
-
-const numbers = [1, 2, 3];
-numbers.push(6);
-
-console.log(numbers);
+const today = new Date();
+console.log(today);
+console.log(typeof today);
