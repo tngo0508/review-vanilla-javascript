d5e8eda25e6b52cdd114f4308d550f38f0034494
Thomas Ngo <tngo0508@gmai.com>
2020-06-18 13:22:56 -0700

    var, let, const

--- app.js
+++ app.js
@@ -1,24 +1,51 @@
-// Log to console
-console.log("hello world");
-console.log(123);
-console.log(true);
-var greeting = "hello";
-console.log(greeting);
-console.log([1, 2, 3, 4]);
-console.log({ a: 1, b: 2 });
-console.table({ a: 1, b: 2 });
+// var, let, const
 
-console.error("this is some error");
-// console.clear();
+// var name = "John Doe";
+// console.log(name);
+// name = "Steve Smith";
+// console.log(name);
 
-console.warn("this is a warning");
-console.time("hello");
-console.log("hello world");
-console.log("hello world");
-console.log("hello world");
-console.log("hello world");
-console.log("hello world");
-console.timeEnd("hello");
-/*
-multi line comments
-*/
+// // Init var
+// var greeting;
+// console.log(greeting);
+// greeting = "Hello";
+// console.log(greeting);
+
+// // letters, numbers, _, $
+// // Cannot start with number
+// var name = "John";
+
+// Multi word vars
+// var firstName = "John"; // Camel case
+// var firstName = "Sara"; // Underscore
+// var FirstName = "Tom"; // Pascal case
+// var firstname;
+
+// LET
+// let name = "John Doe";
+// console.log(name);
+// name = "Steve Smith";
+// console.log(name);
+
+// // CONST
+// const name = "John";
+// console.log(name);
+// // Cannot reassign
+// // name = 'Sara';
+// // Have to assign a value
+// // const greeting;
+
+const person = {
+  name: "John",
+  age: 30,
+};
+
+person.name = "Sara";
+person.age = 32;
+
+// console.log(person);
+
+const numbers = [1, 2, 3];
+numbers.push(6);
+
+console.log(numbers);
