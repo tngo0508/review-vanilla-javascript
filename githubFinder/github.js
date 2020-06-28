5a688de3dbc46b3c933c53570053cfc69be207cc
Thomas Ngo <tngo0508@gmai.com>
2020-06-27 17:43:09 -0700

    finish Github Finder

--- githubFinder/github.js
+++ githubFinder/github.js
@@ -2,6 +2,8 @@ class GitHub {
   constructor() {
     this.client_id = "620c5e2a627e1500363e";
     this.client_secret = "bba6b77f52596617cef3bb5762fd8cd61400b833";
+    this.repos_count = 5;
+    this.repos_sort = "created: asc";
   }
 
   async getUser(user) {
@@ -9,10 +11,16 @@ class GitHub {
       `https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`
     );
 
+    const repoResponse = await fetch(
+      `https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`
+    );
+
     const profile = await profileResponse.json();
+    const repos = await repoResponse.json();
 
     return {
       profile,
+      repos,
     };
   }
 }
